f91315a37d9fa1e752dd475bfcf6238ee1639038
ZeeshanAzhar <101267428+ZeeshanAzhar23@users.noreply.github.com>
2022-07-29 02:14:27 +0500

    Add subscription mailer  for sending mails

---
+++ app/mailers/subscription_mailer.rb
@@ -0,0 +1,13 @@
+class SubscriptionMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.subscription_mailer.payment_failed.subject
+  #
+  def payment_failed
+    @user=params[:user]
+
+    mail to: @user.email, subject: "Payment Attmept Failed"
+  end
+end
