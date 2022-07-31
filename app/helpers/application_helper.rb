8911841aca7bb799bb14be6ee6d83078634e560a
ZeeshanAzhar <101267428+ZeeshanAzhar23@users.noreply.github.com>
2022-07-31 23:46:17 +0500

    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def subed_to_current_plan?(plan,user)
+    Subscription.where(plan_id: plan.id, user_id:user.id).present?
+  end
+  def find_subscribed_plans(user)
+    @subbed_plans = Array.new()
+    @subscriptions=Subscription.where(user_id:user.id)
+    @subscriptions.each do |subscription|
+      @subbed_plans.push(Plan.find_by(id: subscription.plan_id))
+    end
+    @subbed_plans
+  end
 end
