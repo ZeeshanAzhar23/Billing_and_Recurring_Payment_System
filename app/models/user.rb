815b48acb381447508e3f425978a3dc65581f861
ZeeshanAzhar <101267428+ZeeshanAzhar23@users.noreply.github.com>
2022-08-03 12:20:45 +0500

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,54 +1,29 @@
+# frozen_string_literal: true
 class User < ApplicationRecord
-  # include HelperMethods
-  # has_secure_password
-  # # attr_accessor :current_password
-  # validate :current_password_is_correct,
-  #          if: :validate_password?, on: :update
+  include ImageUploader::Attachment(:image)
 
-  enum role: %w{buyer admin}
-  #validations
-  validates_confirmation_of :password
+  enum role: { 'buyer' => 0, 'admin' => 1 }
+  # validations
+  validates :password, confirmation: true
   devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  # Associations
   has_many :subscriptions, dependent: :destroy
   has_many :plans, through: :subscriptions
   has_many :subscriptions, dependent: :destroy
-
-  has_one_attached :avatar
-  # after_commit :add_default_avatar, on: %i[update]
-
-  def avatar_thumbnail
-    if avatar.attached?
-    avatar.variant(resize: '150x150!').processed
-    else
-      '/default_profile.jpg'
-    end
-  end
-
-  # def add_default_avatar
-  #   unless avatar.attached?
-  #     avatar.attach(
-  #       io:File.open(
-  #         Rails.root.join(
-  #         'app', 'assets', 'images', 'default_profile.jpg'
-  #       ), filename: 'default_profile.jpg', content_type: 'image/jpg '
-  #     )
-  #     )
-  #   end
-  # end
-
   def to_s
     email
   end
+  # Callback to Create Stripe customer after creating customer in database
   after_create do
-    customer=Stripe::Customer.create(email: email,name:name)
+    customer = Stripe::Customer.create(email: email, name: name)
     update(customer_id: customer.id)
   end
-  #Find if user has any active subscriptions for billing purposes.
+  # Find if user has any active subscriptions for billing purposes.
   def subscribed?
     subscriptions.where(status: 'active').any?
   end
-
-
+  after_save do
+    image_derivatives! unless image_data.nil?
+  end
 end
