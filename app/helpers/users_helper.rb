c05d5bfc71767a16eb57bbe7b7d0895b74d57acc
ZeeshanAzhar <101267428+ZeeshanAzhar23@users.noreply.github.com>
2022-08-03 12:19:36 +0500

    Update users_helper.rb

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,12 +1,7 @@
+# frozen_string_literal: true
 module UsersHelper
   def not_found
-    raise ActionController::RoutingError.new('Not Found')
-  end
-  #delete customer from stripe if user choses to delete his acc from app
-  def delete_stripe_customer
-    @cust_id=@user.customer_id
-    yield
-    Stripe::Customer.delete(@cust_id)
+    raise ActionController::RoutingError, 'Not Found'
   end
 
 end
