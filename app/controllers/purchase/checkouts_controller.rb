b54cbbfefb906ed9260d4d65626a1fe05e2f139d
ZeeshanAzhar <101267428+ZeeshanAzhar23@users.noreply.github.com>
2022-08-03 13:04:41 +0500

    Update checkouts_controller.rb

--- app/controllers/purchase/checkouts_controller.rb
+++ app/controllers/purchase/checkouts_controller.rb
@@ -1,41 +1,27 @@
-class Purchase::CheckoutsController < ApplicationController
-  #before_action :authenticate_user!
- # before_action :retrieve_price_id, only: [:create]
-  def create
-      @plan=Plan.find(params[:plan_id])
-      price=@plan.price_id
-      #price = params[:pricing_id]
+# frozen_string_literal: true
+module Purchase
+  class CheckoutsController < ApplicationController
+    def create
+      @plan = Plan.find(params[:plan_id])
+      price = @plan.price_id
+      # price = params[:pricing_id]
       session = Stripe::Checkout::Session.create(
         customer: current_user.customer_id,
         client_reference_id: current_user.id,
-        success_url: root_url + 'success?session_id={CHECKOUT_SESSION_ID}',
+        success_url: "#{root_url}success?session_id={CHECKOUT_SESSION_ID}",
         cancel_url: root_url,
         payment_method_types: ['card'],
         mode: 'subscription',
-        # customer_email: current_user.email,
         line_items: [{
           quantity: 1,
-          price: price,
-          #price: price,
-
+          price: @plan.price_id
         }]
       )
-
       redirect_to session.url, allow_other_host: true
+    end
+    def success
+      session = Stripe::Checkout::Session.retrieve(params[:session_id])
+      @customer = Stripe::Customer.retrieve(session.customer)
+    end
   end
-
-  def success
-    session = Stripe::Checkout::Session.retrieve(params[:session_id])
-    @customer = Stripe::Customer.retrieve(session.customer)
-  end
-
-  # def retrieve_price_id
-  #   # price=Stripe::Price.search({
-  #   #   query: 'active:\'true\' AND metadata[\'order_id\']:\'6735\'',
-  #   # }).data[9]
-  #   p=Plan.new
-  #   puts p.return_price_id
-  #   puts "checking price"
-  # end
-
 end
