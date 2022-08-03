a07a5b6ffe4bf14aa8f5256e61695fe947087885
ZeeshanAzhar <101267428+ZeeshanAzhar23@users.noreply.github.com>
2022-08-03 12:18:43 +0500

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,54 +1,52 @@
+# frozen_string_literal: true
 class UsersController < ApplicationController
-  before_action :find_user, only: [:show,:destroy,:edit,:update]
+  before_action :find_user, only: %i[show destroy edit update]
   around_action :delete_stripe_customer, only: [:destroy]
-  #before_action :authorize_admin, only: [:index]
-
-
-
+  before_action :authenticate_user!
+  after_action  :update_stripe_customer, only: [:update]
   def index
-    @users= User.all
+    @users = User.all
     authorize @users
   end
-
-  def show
-
-
-  end
-
+  def show; end
   def edit
-    # @user=User.find(params[:id])
     authorize @user
   end
-
   def update
-    @user=User.find(params[:id])
     authorize @user
-    if @user.update(user_params)
+    if @user.update!(user_params)
       redirect_to @user
     else
       render 'edit'
     end
   end
-
   def destroy
-    if @user.destroy
-      redirect_to users_path
-    end
-
+    redirect_to users_path if @user.destroy
   end
-
-  private def user_params
-    params.require(:user).permit(:name,:email)
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :image)
   end
-
-  private def find_user
-    @user=User.find(params[:id])
+  def find_user
+    @user = User.find(params[:id])
   end
-
-  private def authorize_admin
-    return unless !current_user.admin?
+  def authorize_admin
+    return if current_user.admin?
     redirect_to root_path, alert: 'Admins only!'
   end
-
-
+  def delete_stripe_customer
+    @cust_id = @user.customer_id
+    yield
+    Stripe::Customer.delete(@cust_id)
+  end
+  def update_stripe_customer
+    Rails.logger.debug @user.customer_id
+    if @user.role == 'buyer'
+      Stripe::Customer.update(
+        @user.customer_id,
+        email: @user.email,
+        name: @user.name
+      )
+    end
+  end
 end
