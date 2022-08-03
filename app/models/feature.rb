63adbbc327862a32f3b30aaa99dc962453b94586
ZeeshanAzhar <101267428+ZeeshanAzhar23@users.noreply.github.com>
2022-08-03 12:59:29 +0500

    Update feature.rb

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,14 +1,11 @@
+# frozen_string_literal: true
 class Feature < ApplicationRecord
   belongs_to :plan
-  #validations
-  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 20}
-  validates :code, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 20}
-  validates :unit_price, presence: true, length: {maximum: 10}
-  validates :max_unit_limit, presence: true,length: {maximum: 20}
-
-
-
-
-
-
+  has_one :feature_usage, dependent: :destroy
+  # validations
+  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 20 }
+  validates :code, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 20 } ,
+                                                               numericality: {only_integer: true}
+  validates :unit_price, presence: true, length: { maximum: 10 }, numericality: true
+  validates :max_unit_limit, presence: true, length: { maximum: 20 }, numericality: true
 end
