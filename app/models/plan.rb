ef10e7ff8d3fedca2cf0ff2385464a9b46c41d7d
ZeeshanAzhar <101267428+ZeeshanAzhar23@users.noreply.github.com>
2022-08-03 12:39:12 +0500

    Update plan.rb

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,30 +1,27 @@
+# frozen_string_literal: true
 class Plan < ApplicationRecord
+  include PlanConcern
+  #associations
   has_many :features, dependent: :destroy
   has_many :users, through: :subscriptions
   has_many :subscriptions, dependent: :nullify
-  #validations
-  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 50}
-  validates :monthly_fee, presence: true, length: {maximum: 20}
-  validates_numericality_of :monthly_fee, less_than_or_equal_to: BigDecimal(10**8)
-  #@price
+  # validations
+  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 50 }
+  validates :monthly_fee, presence: true, length: { maximum: 20 }
+  validates :monthly_fee, numericality: { less_than_or_equal_to: BigDecimal(10**8) }
   attr_reader :price
+  #model callbacks
   after_create do
-    @plan=Stripe::Product.create({name: name,})
+    @plan = Stripe::Product.create({ name: name })
+    @flag = true
     update(stripe_plan_id: @plan.id)
-    @price=Stripe::Price.create({
-      product: @plan.id,
-      unit_amount: monthly_fee.to_i,
-      currency:'usd',
-      recurring: {interval: 'month'},
-      metadata: {order_id: '6735'},
-    })
-    update(price_id: @price.id)
   end
-
-  # def return_price_id
-  #   puts 'chcking'
-  #   @price.id
-  #   puts @price.id
-  # end
-
+  after_update do
+    unless @flag
+      Stripe::Product.update(
+        stripe_plan_id,
+        name: name
+      )
+    end
+  end
 end
